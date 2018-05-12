3236ef626676d4d006eff31c4289e5b8b1ed7ed4
Dang Bao <dangbaoxx@gmail.com>
2018-05-12 08:25:44 +0700

    Xong bảng VT

--- QuanLyKho/QuanLyKho/frmVatTu.Designer.cs
+++ QuanLyKho/QuanLyKho/frmVatTu.Designer.cs
@@ -29,22 +29,22 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnRS = new System.Windows.Forms.Button();
+            this.btnXoaVT = new System.Windows.Forms.Button();
+            this.btnSuaVT = new System.Windows.Forms.Button();
+            this.btnThemVT = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbTenVT = new System.Windows.Forms.TextBox();
+            this.txbXX = new System.Windows.Forms.TextBox();
+            this.txbMaVT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvVT = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txbMaVT = new System.Windows.Forms.TextBox();
-            this.txbXX = new System.Windows.Forms.TextBox();
-            this.txbTenVT = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnThemVT = new System.Windows.Forms.Button();
-            this.btnSuaVT = new System.Windows.Forms.Button();
-            this.btnXoaVT = new System.Windows.Forms.Button();
-            this.btnRS = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -81,87 +81,49 @@
             this.splitContainer1.SplitterDistance = 332;
             this.splitContainer1.TabIndex = 0;
             // 
-            // groupBox1
+            // btnRS
             // 
-            this.groupBox1.Controls.Add(this.listView1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(661, 566);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Thông tin vật tư";
+            this.btnRS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRS.Location = new System.Drawing.Point(176, 457);
+            this.btnRS.Name = "btnRS";
+            this.btnRS.Size = new System.Drawing.Size(101, 44);
+            this.btnRS.TabIndex = 15;
+            this.btnRS.Text = "Reset";
+            this.btnRS.UseVisualStyleBackColor = true;
+            this.btnRS.Click += new System.EventHandler(this.btnRS_Click);
             // 
-            // listView1
+            // btnXoaVT
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(3, 23);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(655, 540);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.btnXoaVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoaVT.Location = new System.Drawing.Point(34, 457);
+            this.btnXoaVT.Name = "btnXoaVT";
+            this.btnXoaVT.Size = new System.Drawing.Size(101, 44);
+            this.btnXoaVT.TabIndex = 14;
+            this.btnXoaVT.Text = "Xóa";
+            this.btnXoaVT.UseVisualStyleBackColor = true;
+            this.btnXoaVT.Click += new System.EventHandler(this.btnXoaVT_Click);
             // 
-            // label3
+            // btnSuaVT
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(92, 34);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(131, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Nhập thông tin";
+            this.btnSuaVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuaVT.Location = new System.Drawing.Point(176, 383);
+            this.btnSuaVT.Name = "btnSuaVT";
+            this.btnSuaVT.Size = new System.Drawing.Size(101, 44);
+            this.btnSuaVT.TabIndex = 13;
+            this.btnSuaVT.Text = "Sửa";
+            this.btnSuaVT.UseVisualStyleBackColor = true;
+            this.btnSuaVT.Click += new System.EventHandler(this.btnSuaVT_Click);
             // 
-            // label1
+            // btnThemVT
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 18);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Mã vật tư";
-            // 
-            // txbMaVT
-            // 
-            this.txbMaVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbMaVT.Location = new System.Drawing.Point(34, 120);
-            this.txbMaVT.Name = "txbMaVT";
-            this.txbMaVT.Size = new System.Drawing.Size(243, 24);
-            this.txbMaVT.TabIndex = 7;
-            // 
-            // txbXX
-            // 
-            this.txbXX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbXX.Location = new System.Drawing.Point(34, 290);
-            this.txbXX.Name = "txbXX";
-            this.txbXX.Size = new System.Drawing.Size(243, 24);
-            this.txbXX.TabIndex = 8;
-            // 
-            // txbTenVT
-            // 
-            this.txbTenVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTenVT.Location = new System.Drawing.Point(34, 204);
-            this.txbTenVT.Name = "txbTenVT";
-            this.txbTenVT.Size = new System.Drawing.Size(243, 24);
-            this.txbTenVT.TabIndex = 9;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(31, 183);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 18);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Tên vật tư";
+            this.btnThemVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemVT.Location = new System.Drawing.Point(34, 383);
+            this.btnThemVT.Name = "btnThemVT";
+            this.btnThemVT.Size = new System.Drawing.Size(101, 44);
+            this.btnThemVT.TabIndex = 12;
+            this.btnThemVT.Text = "Thêm";
+            this.btnThemVT.UseVisualStyleBackColor = true;
+            this.btnThemVT.Click += new System.EventHandler(this.btnThemVT_Click);
             // 
             // label4
             // 
@@ -173,45 +135,105 @@
             this.label4.TabIndex = 11;
             this.label4.Text = "Xuất xứ";
             // 
-            // btnThemVT
+            // label2
             // 
-            this.btnThemVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThemVT.Location = new System.Drawing.Point(34, 383);
-            this.btnThemVT.Name = "btnThemVT";
-            this.btnThemVT.Size = new System.Drawing.Size(101, 44);
-            this.btnThemVT.TabIndex = 12;
-            this.btnThemVT.Text = "Thêm";
-            this.btnThemVT.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tên vật tư";
             // 
-            // btnSuaVT
+            // txbTenVT
             // 
-            this.btnSuaVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSuaVT.Location = new System.Drawing.Point(176, 383);
-            this.btnSuaVT.Name = "btnSuaVT";
-            this.btnSuaVT.Size = new System.Drawing.Size(101, 44);
-            this.btnSuaVT.TabIndex = 13;
-            this.btnSuaVT.Text = "Sửa";
-            this.btnSuaVT.UseVisualStyleBackColor = true;
+            this.txbTenVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTenVT.Location = new System.Drawing.Point(34, 204);
+            this.txbTenVT.Name = "txbTenVT";
+            this.txbTenVT.Size = new System.Drawing.Size(243, 24);
+            this.txbTenVT.TabIndex = 9;
             // 
-            // btnXoaVT
+            // txbXX
             // 
-            this.btnXoaVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnXoaVT.Location = new System.Drawing.Point(34, 457);
-            this.btnXoaVT.Name = "btnXoaVT";
-            this.btnXoaVT.Size = new System.Drawing.Size(101, 44);
-            this.btnXoaVT.TabIndex = 14;
-            this.btnXoaVT.Text = "Xóa";
-            this.btnXoaVT.UseVisualStyleBackColor = true;
+            this.txbXX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbXX.Location = new System.Drawing.Point(34, 290);
+            this.txbXX.Name = "txbXX";
+            this.txbXX.Size = new System.Drawing.Size(243, 24);
+            this.txbXX.TabIndex = 8;
             // 
-            // btnRS
+            // txbMaVT
             // 
-            this.btnRS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRS.Location = new System.Drawing.Point(176, 457);
-            this.btnRS.Name = "btnRS";
-            this.btnRS.Size = new System.Drawing.Size(101, 44);
-            this.btnRS.TabIndex = 15;
-            this.btnRS.Text = "Reset";
-            this.btnRS.UseVisualStyleBackColor = true;
+            this.txbMaVT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMaVT.Location = new System.Drawing.Point(34, 120);
+            this.txbMaVT.Name = "txbMaVT";
+            this.txbMaVT.Size = new System.Drawing.Size(243, 24);
+            this.txbMaVT.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mã vật tư";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nhập thông tin";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lvVT);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(661, 566);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin vật tư";
+            // 
+            // lvVT
+            // 
+            this.lvVT.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvVT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvVT.FullRowSelect = true;
+            this.lvVT.GridLines = true;
+            this.lvVT.Location = new System.Drawing.Point(3, 23);
+            this.lvVT.Name = "lvVT";
+            this.lvVT.Size = new System.Drawing.Size(655, 540);
+            this.lvVT.TabIndex = 0;
+            this.lvVT.UseCompatibleStateImageBehavior = false;
+            this.lvVT.View = System.Windows.Forms.View.Details;
+            this.lvVT.SelectedIndexChanged += new System.EventHandler(this.lvVT_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã vật tư";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên vật tư";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Xuất Xứ";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 150;
             // 
             // frmVatTu
             // 
@@ -237,7 +259,7 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lvVT;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
--- QuanLyKho/QuanLyKho/frmVatTu.cs
+++ QuanLyKho/QuanLyKho/frmVatTu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,218 @@ namespace QuanLyKho
         {
             InitializeComponent();
         }
+        DataConections connect = new DataConections();
+        List<string> list = new List<string>();
+        #region LoadData_in_database
+        public void ShowVT()
+        {
+            btnSuaVT.Enabled = false;
+            btnXoaVT.Enabled = false;
+            connect.OpenConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from VATTU";
+            cmd.Connection = connect.conn;
 
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ListViewItem listview = new ListViewItem(reader.GetString(0));
+                listview.SubItems.Add(reader.GetString(1));
+                listview.SubItems.Add(reader.GetString(2));
+                list.Add(reader.GetString(0));
+                lvVT.Items.Add(listview);
+            }
+            reader.Close();
+        }
         private void frmVatTu_Load(object sender, EventArgs e)
         {
+            ShowVT();
+        }
+        #endregion
+
+        #region event_click_listview
+        private void lvVT_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txbMaVT.Enabled = false;
+            btnThemVT.Enabled = false;
+            btnSuaVT.Enabled = true;
+            btnXoaVT.Enabled = true;
+            if (lvVT.SelectedItems.Count == 0) return;
+            ListViewItem listview = lvVT.SelectedItems[0];
+            txbMaVT.Text = listview.SubItems[0].Text;
+            txbTenVT.Text = listview.SubItems[1].Text;
+            txbXX.Text = listview.SubItems[2].Text;
+        }
+
+        #endregion
+
+        #region event_buttons_click
+        private void btnThemVT_Click(object sender, EventArgs e)
+        {
+            bool check = true;
+            if (txbMaVT.Text != "")
+            {
+                foreach (string us in list)
+                {
+                    if (us.Contains(txbMaVT.Text))
+                    {
+                        MessageBox.Show("Mã kho đã tồn tại!", "Thông báo");
+                        check = false;
+                        break;
+                    }
+                    check = true;
+                }
+                if (check == true)
+                {
+                    ListViewItem lv = new ListViewItem(txbMaVT.Text);
+                    lv.SubItems.Add(txbTenVT.Text);
+                    lv.SubItems.Add(txbXX.Text);
+                    lvVT.Items.Add(lv);
+                    ThemVT();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Chưa nhập mã vật tư!", "Thông báo");
+            }
+        }
+
+        private void btnSuaVT_Click(object sender, EventArgs e)
+        {
+            btnThemVT.Enabled = true;
+            if (lvVT.SelectedItems.Count == 0) return;
+            ListViewItem lv = lvVT.SelectedItems[0];
+            lv.SubItems[0].Text = txbMaVT.Text;
+            lv.SubItems[1].Text = txbTenVT.Text;
+            lv.SubItems[2].Text = txbXX.Text;
+            SuaVT();
+        }
+
+        private void btnXoaVT_Click(object sender, EventArgs e)
+        {
+            if (lvVT.SelectedItems != null)
+            {
+                for (int i = 0; i < lvVT.Items.Count; i++)
+                {
+                    if (lvVT.Items[i].Selected)
+                    {
+                        lvVT.Items[i].Remove();
+                        i--;
+                    }
+                }
+                XoaVT();
+            }
+        }
+
+        private void btnRS_Click(object sender, EventArgs e)
+        {
+            btnThemVT.Enabled = true;
+            btnSuaVT.Enabled = false;
+            btnXoaVT.Enabled = false;
+            txbMaVT.Enabled = true;
+            txbMaVT.ResetText();
+            txbTenVT.ResetText();
+            txbXX.ResetText();
+        }
+        #endregion
+
+        #region Database
+        private void ThemVT()
+        {
+            DialogResult dlr = MessageBox.Show("Bạn chắc chắn muốn thêm không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dlr == DialogResult.Yes)
+            {
+                connect.OpenConnection();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "ThemVT";
+                cmd.Connection = connect.conn;
+                cmd.Parameters.Add("@MAVT", SqlDbType.VarChar).Value = txbMaVT.Text;
+                cmd.Parameters.Add("@TENVT", SqlDbType.NVarChar).Value = txbTenVT.Text;
+                cmd.Parameters.Add("@XUATXU", SqlDbType.NVarChar).Value = txbXX.Text;
+                int ret = cmd.ExecuteNonQuery();
+                lvVT.Items.Clear();
+                if (ret > 0)
+                    ShowVT();
+
+                MessageBox.Show("Đã thêm thành công", "Thêm");
+
+                txbMaVT.ResetText();
+                txbTenVT.ResetText();
+                txbXX.ResetText();
+            }
+            else
+            {
+                lvVT.Items.Clear();
+                ShowVT();
+            }
+        }
+        private void SuaVT()
+        {
+            DialogResult dlr = MessageBox.Show("Bạn chắc chắn muốn sửa ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dlr == DialogResult.Yes)
+            {
+                connect.OpenConnection();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SuaVT";
+                cmd.Connection = connect.conn;
+                cmd.Parameters.Add("@MAVT", SqlDbType.VarChar).Value = txbMaVT.Text;
+                cmd.Parameters.Add("@TENVT", SqlDbType.NVarChar).Value = txbTenVT.Text;
+                cmd.Parameters.Add("@XUATXU", SqlDbType.NVarChar).Value = txbXX.Text;
+
+                int ret = cmd.ExecuteNonQuery();
+                lvVT.Items.Clear();
+                if (ret > 0)
+                    ShowVT();
+                MessageBox.Show("Đã sửa thành công", "Sửa");
+            }
+            else
+            {
+                lvVT.Items.Clear();
+                ShowVT();
+            }
 
         }
+        public void XoaVT()
+        {
+            DialogResult dlr = MessageBox.Show("Bạn chắc chắn muốn sửa ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dlr == DialogResult.Yes)
+            {
+                connect.OpenConnection();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "XoaVT";
+                cmd.Connection = connect.conn;
+
+                cmd.Parameters.Add("@MAVT", SqlDbType.VarChar).Value = txbMaVT.Text;
+                int ret = cmd.ExecuteNonQuery();
+                lvVT.Items.Clear();
+                if (ret > 0)
+                    ShowVT();
+                MessageBox.Show("Đã xóa thành công", "Xóa");
+                btnThemVT.Enabled = true;
+                btnSuaVT.Enabled = false;
+                btnXoaVT.Enabled = false;
+                txbMaVT.Enabled = true;
+                txbMaVT.ResetText();
+                txbTenVT.ResetText();
+                txbXX.ResetText();
+            }
+            else
+            {
+                btnThemVT.Enabled = true;
+                btnSuaVT.Enabled = false;
+                btnXoaVT.Enabled = false;
+                txbMaVT.Enabled = true;
+                txbMaVT.ResetText();
+                txbTenVT.ResetText();
+                txbXX.ResetText();
+                lvVT.Items.Clear();
+                ShowVT();
+            }
+        }
+        #endregion
     }
 }
